43ba054dbb8e14a36be7bc8e1f59c616a1e0ab75
khaloj <joey.khalaf@etu.emse.fr>
2023-12-08 13:48:21 +0100

    Added env port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ToiletModule } from './toilet.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(ToiletModule);
-  await app.listen(3000);
+  await app.listen(PORT ?? 3000);
 }
 bootstrap();
